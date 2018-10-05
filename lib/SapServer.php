2b338778c49fba01cb50978436d3b5e3c094648f
Pablo Iglesias <pablo.iglesias02@gmail.com>
2018-10-05 15:02:59 +0100

    Added method to send usage

--- lib/SapServer.php
+++ lib/SapServer.php
@@ -15,6 +15,8 @@ use League\OAuth1\Client\Signature\HmacSha1Signature;
 class SapServer extends Server {
 
 	const PATTERN = '/OAuth oauth_consumer_key=".*?", oauth_nonce=".*?", oauth_signature=".*?", oauth_signature_method="HMAC-SHA1", oauth_timestamp="\d{10}", oauth_version="1.0"/';
+	const METERED_USAGE_URL_DEV = 'https://nextgen.sapappcenter.com/api/integration/v1/billing/usage';
+	const METERED_USAGE_URL = 'https://www.sapappcenter.com/api/integration/v1/billing/usage';
 
 	public function urlTemporaryCredentials() {
 		// TODO: Implement urlTemporaryCredentials() method.
@@ -52,7 +54,6 @@ class SapServer extends Server {
 		$client = $this->createHttpClient();
 
 		$headers = $this->getHeaders($this->getClientCredentials(), 'GET', $url);
-
 		try {
 			$response = $client->get($url, [
 				'headers' => $headers,
@@ -81,6 +82,43 @@ class SapServer extends Server {
 
 		return $resp;
 	}
+
+	public function sendMeteredUsage($data, $type = "json", $dev = FALSE) {
+        $client = $this->createHttpClient();
+
+        $url = (!@$dev) ? self::METERED_USAGE_URL : self::METERED_USAGE_URL_DEV;
+
+        $headers = $this->getHeaders($this->getClientCredentials(), 'POST', $url);
+
+        try {
+            $response = $client->post($url, [
+                'headers' => $headers,
+                'json' => $data
+            ]);
+        } catch (BadResponseException $e) {
+            $response = $e->getResponse();
+            $body = $response->getBody();
+            $statusCode = $response->getStatusCode();
+
+            throw new \Exception(
+                "Received error [$body] with status code [$statusCode] when retrieving token credentials."
+            );
+        }
+
+        switch ($type) {
+            case 'json':
+                $resp = json_decode((string) $response->getBody(), true);
+                break;
+
+            case 'xml':
+                $resp = simplexml_load_string((string) $response->getBody());
+                break;
+            default:
+                throw new \InvalidArgumentException("Invalid response type [{$this->responseType}].");
+        }
+
+        return $resp;
+    }
 	/**
 	 * Generate the OAuth protocol header for requests other than temporary
 	 * credentials, based on the URI, method, given credentials & body query
